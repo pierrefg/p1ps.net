a5f913f7bffe9794fc00425b4e5eb9a5b6af6a24
Pierrefg <pierrefg@proton.me>
2025-10-22 10:38:27 +0200

    New appearance

--- app/layout.js
+++ app/layout.js
@@ -1,18 +1,7 @@
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
 export const metadata = {
-  title: "p1ps_is_alive",
+  title: "p1ps_is_alive;",
   description: "p1ps_is_alive",
 };
 
--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center">
-      <h1>p1ps_is_alive;</h1>
+      p1ps_is_alive;
     </div>
   );
 }
